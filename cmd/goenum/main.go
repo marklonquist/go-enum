4bd76bb832b7128f24c06abdfaab0af4c2b93de9
Mark Bøg Lønquist <mark@fundayfactory.com>
2020-01-28 09:48:28 +0100

    first commit

---
+++ cmd/goenum/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+func main() {
+	help := flag.Bool("help", true, "Help")
+	path := flag.String("path", "", "path")
+	debug := flag.Bool("debug", false, "Debug mode")
+	flag.Parse()
+
+	if *help && *path == "" {
+		log.Println("goenum --path <PATH> (--debug)")
+		return
+	}
+
+	if *path == "" {
+		log.Fatal("Path is a required parameter; --path <PATH>")
+	}
+
+	generate(*path, *debug)
+}
+
+func generate(p string, debug bool) {
+	funcs := template.FuncMap{"join": strings.Join}
+	t, _ := template.New("").Funcs(funcs).Parse(tmpl)
+
+	fileSet := token.NewFileSet()
+	err := filepath.Walk(p, func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(path, ".go") {
+			f, err := parser.ParseFile(fileSet, path, nil, parser.ParseComments)
+			if err == nil {
+				for _, c := range f.Comments {
+					for _, c1 := range c.List {
+						if c1.Text == "// x-enum" {
+							dir, _ := filepath.Split(path)
+							d, err := parser.ParseDir(fileSet, dir, nil, parser.PackageClauseOnly)
+							if err != nil {
+								return err
+							}
+							pkgName := ""
+							for k, _ := range d {
+								pkgName = k
+							}
+
+							typeName := ""
+							m := make(map[int]string)
+							for k, v := range f.Scope.Objects {
+								if v.Kind == ast.Typ {
+									typeName = k
+								}
+								if v.Kind == ast.Con {
+									m[v.Data.(int)] = k
+								}
+							}
+
+							tmplModel := tmplModel{
+								PkgName:  pkgName,
+								Enums:    make([]EnumModel, len(m)),
+								TypeName: typeName,
+							}
+
+							for k, v := range m {
+								tmplModel.Enums[k] = EnumModel{
+									Word:     v,
+									TypeName: typeName,
+								}
+							}
+
+							newFile, _ := os.Create(dir + pkgName + ".gen.go")
+							w := bufio.NewWriter(newFile)
+							err = t.Execute(w, tmplModel)
+							if err != nil {
+								log.Println(err)
+							}
+							w.Flush()
+
+							if debug {
+								log.Println("Generated for " + typeName + " in path: " + path)
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return err
+	})
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+type EnumModel struct {
+	Word     string
+	TypeName string
+}
+
+type tmplModel struct {
+	PkgName  string
+	Enums    []EnumModel
+	TypeName string
+}
+
+const tmpl = `// Generated code. DO NOT EDIT.
+package {{ .PkgName }}
+
+import "errors"
+
+func (kind {{ .TypeName }}) String() string {
+	switch kind { {{ range $index, $element := .Enums }}
+	case {{ $index }}:
+		return "{{$element.Word}}" {{ end }}
+	default:
+		return ""
+	}
+}
+
+func Parse(name string) ({{ .TypeName }}, error) {
+	switch name { {{ range $index, $element := .Enums }}
+	case "{{$element.Word}}":
+		return {{ $element.TypeName }}({{ $index }}), nil {{ end }}
+	default:
+		return {{ .TypeName }}(0), errors.New("Enum for \"{{ .TypeName }}\" not found using name = " + name)
+	}
+}`
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/marklonquist/go-enum
+
+go 1.13
+
+require github.com/urfave/cli v1.22.2
